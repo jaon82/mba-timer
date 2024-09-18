e7cdcd1a1673bbc5ea4c1773b7430cbfb7cf9de8
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-18 00:08:48 -0400

    Add font and colors

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -16,12 +16,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme["green-500"]};
   color: ${(props) => props.theme.white};
 
-  ${(props) => {
+  /*${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `;
-  }}
+  }}*/
 `;
