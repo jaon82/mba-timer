e209ffc0c800d9051c3beb8132942cf8aad3b964
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-17 23:58:36 -0400

    Add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button />
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
