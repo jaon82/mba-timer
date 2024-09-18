16dfc81cc3aee04d4b43037e4e0e758b5cf0d5a8
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-17 23:42:06 -0400

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import Button from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -16,6 +16,8 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
 
   ${(props) => {
     return css`
