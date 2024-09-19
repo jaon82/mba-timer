5c1be345381a664bba76003a22429e9d3036a95f
Jean Nunes <jean.nunes@uzzipay.com>
2024-09-18 23:20:07 -0400

    Add routes and layout

--- src/components/Button/index.tsx
+++
@@ -1,8 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./styled";
-
-interface ButtonProps {
-  variant?: ButtonVariant;
-}
-export default function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>;
-}
